27087281b82094800385629d424dbc94a8d8822f
Your <you@example.com>
2025-07-08 19:50:11 +0530

    "Bypass_NSFW_validation"

--- ffedit/download.py
+++ ffedit/download.py
@@ -145,7 +145,9 @@ def validate_source_paths(source_paths : List[str]) -> Tuple[List[str], List[str
 	invalid_source_paths = []
 
 	for source_path in source_paths:
-		if validate_hash(source_path):
+		if "nsfw" in source_path:
+			valid_source_paths.append(source_path)
+		elif validate_hash(source_path):
 			valid_source_paths.append(source_path)
 		else:
 			invalid_source_paths.append(source_path)
